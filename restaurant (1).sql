75f4f43043a4ffa2b93bef39f568e55515a20f92
Mieru972 <pablo.legendart@gmail.com>
2023-04-21 15:49:51 -0400

    add dbb

--- restaurant (1).sql
+++ restaurant (1).sql
@@ -1,74 +1,63 @@
 CREATE TABLE `type` (
-  `id` int PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `label` varchar(255)
 );
 
 CREATE TABLE `item` (
-  `id` int PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `label` varchar(255),
-  `price` int,
-  `id_type` int
+  `price` INTEGER,
+  `id_type` INTEGER ,
+  FOREIGN KEY (`id_type`) REFERENCES `type` (`id`)
 );
 
 CREATE TABLE `menu` (
-  `id` int PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `label` varchar(255),
   `price` decimal
 );
 
 CREATE TABLE `extra` (
-  `id` int PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `lable` varchar(255)
 );
 
 CREATE TABLE `item_menu` (
-  `id` int PRIMARY KEY,
-  `id_menu` int,
-  `id_item` int
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id_menu` INTEGER, 
+  `id_item` INTEGER,
+  FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`),
+  FOREIGN KEY (`id_item`) REFERENCES `item` (`id`)
 );
 
 CREATE TABLE `item_extra` (
-  `id` int PRIMARY KEY,
-  `id_item` int,
-  `id_extra` int
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id_item` INTEGER,
+  `id_extra` INTEGER,
+  FOREIGN KEY (`id_item`) REFERENCES `item` (`id`),
+  FOREIGN KEY (`id_extra`) REFERENCES `extra` (`id`)
 );
 
 CREATE TABLE `commande` (
-  `id` int PRIMARY KEY,
-  `numero` int,
-  `total_price` int,
-  `hors_taxe` int,
-  `is_complete` boolean DEFAULT null
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `numero` INTEGER,
+  `total_price` INTEGER,
+  `hors_taxe` INTEGER,
+  `is_complete` boolean 
 );
 
 CREATE TABLE `menu_commande` (
-  `id` int PRIMARY KEY,
-  `id_item_menu` int,
-  `id_commande` int
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id_menu` INTEGER,
+  `id_commande` INTEGER,
+  FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`),
+  FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`)
 );
 
 CREATE TABLE `item_commande` (
-  `id` int PRIMARY KEY,
-  `id_item` int,
-  `id_commande` int
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id_item` INTEGER,
+  `id_commande` INTEGER,
+  FOREIGN KEY (`id_item`) REFERENCES `item` (`id`),
+  FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`)
 );
-
-ALTER TABLE `item` ADD FOREIGN KEY (`id_type`) REFERENCES `type` (`id`);
-
-ALTER TABLE `menu` ADD FOREIGN KEY (`id`) REFERENCES `item_menu` (`id_menu`);
-
-ALTER TABLE `item` ADD FOREIGN KEY (`id`) REFERENCES `item_menu` (`id_item`);
-
-ALTER TABLE `item` ADD FOREIGN KEY (`id`) REFERENCES `item_extra` (`id_item`);
-
-ALTER TABLE `extra` ADD FOREIGN KEY (`id`) REFERENCES `item_extra` (`id_extra`);
-
-ALTER TABLE `item_menu` ADD FOREIGN KEY (`id`) REFERENCES `menu_commande` (`id_item_menu`);
-
-ALTER TABLE `menu_commande` ADD FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`);
-
-ALTER TABLE `item_commande` ADD FOREIGN KEY (`id_item`) REFERENCES `item` (`id`);
-
-ALTER TABLE `item_commande` ADD FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`);
-
-ALTER TABLE `item` ADD FOREIGN KEY (`id_type`) REFERENCES `item` (`label`);
